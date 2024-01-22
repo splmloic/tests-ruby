9258cd6b04b1e899530a4811c58b6861c4a3a3f1
splmloic <l2ct@protonmail.com>
2024-01-22 11:51:05 +0100

    Formule de conversion Farenheit <=> Celsus avec commentraire

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -2,7 +2,8 @@ def ftoc (f)#farenheit to Celsus
     c = (f-32)*10/18
     return c
 end
-
+#ma solution pour cette partie a été de multiplier par 10 afin de pouvoir non pas diviser par 1.8 mais par 18
+#ce qui permet d'obtenir un entier dans le calcul de body temperature
 def ctof (c)#Celsus to ferenheit
     f = (c*1.8)+32
     return f
