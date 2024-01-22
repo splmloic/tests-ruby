44db1beebbd14d7698e8e6e3f62e466e12ce1ab6
splmloic <l2ct@protonmail.com>
2024-01-22 15:17:39 +0100

    Exercice numéro 2 sur le sujet de la calculete

---
+++ lib/02_calculator.rb
@@ -0,0 +1,19 @@
+def add(a, b)
+    return a + b
+end
+
+def subtract(a, b)
+    return a - b
+end
+
+def sum(a=[])
+    return a.sum
+end
+
+def multiply(a, b)
+    return a * b
+end
+
+def factorial (a)
+    return (1..a).reduce(:*) || 1
+end
